310f174432a734d084ee2bc12c1acd1915c461af
VANSH KAPOOR <vanshkapoorvk7@gmail.com>
2018-12-30 22:29:38 +0530

    backward elimination

---
+++ mlr.py
@@ -0,0 +1,56 @@
+#multiple linear regression
+
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv('50_Startups.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:,4].values
+
+#encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x = LabelEncoder()
+x[:,3] = labelencoder_x.fit_transform(x[:,3])
+
+onehotencoder = OneHotEncoder(categorical_features=[3])
+x = onehotencoder.fit_transform(x).toarray()
+
+#avoiding dummy variable trap
+x = x[:,1:]
+
+#splitting
+from sklearn.cross_validation import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2,random_state = 0)
+
+
+#linear regressor
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train,y_train)
+
+y_pred = regressor.predict(x_test)
+
+#adding x0 = 1
+x = np.append(arr = np.ones((50,1)).astype(int), values = x, axis = 1)
+
+import statsmodels.formula.api as sm
+x_opt = x[:,[0,1,2,3,4,5]]
+regressor_ols = sm.OLS(endog = y , exog = x_opt).fit()
+regressor_ols.summary()
+
+x_opt = x[:,[0,1,3,4,5]]
+regressor_ols = sm.OLS(endog = y , exog = x_opt).fit()
+regressor_ols.summary()
+
+x_opt = x[:,[0,3,4,5]]
+regressor_ols = sm.OLS(endog = y , exog = x_opt).fit()
+regressor_ols.summary()
+
+x_opt = x[:,[0,3,5]]
+regressor_ols = sm.OLS(endog = y , exog = x_opt).fit()
+regressor_ols.summary()
+
+
+x_opt = x[:,[0,3]]
+regressor_ols = sm.OLS(endog = y , exog = x_opt).fit()
+regressor_ols.summary()
